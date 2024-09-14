c1994209a44ab8bb1a1af3d17858883c34c88248
RNito02 <02rnito02@gmail.com>
2024-09-14 14:37:13 -0600

    Respaldo

---
+++ personaje.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+class Cubo:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.ancho = 50
+        self.alto = 50
+        self.velocidad = 1
+        self.color = (255, 0, 0)  # Definir el color rojo en RGB
+        self.rect = pygame.Rect(self.x, self.y, self.ancho, self.alto)
+
+    def dibujar(self, ventana):  # Cambié "seft" a "self"
+        pygame.draw.rect(ventana, self.color, self.rect)
